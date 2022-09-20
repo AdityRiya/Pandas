2cc2b954a6ca057227578f7ddfb693938090642c
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-09-20 23:49:34 +0600

    3rd

--- pandas5.py
+++ pandas5.py
@@ -1,3 +1,8 @@
-import pandas as pd
-iris=pd.read_csv('pdcsv.csv')
-print(iris.head())
+# import pandas as pd
+# iris=pd.read_csv('pdcsv.csv')
+# print(iris.head())
+import pandas as pd 
+s1=pd.DataFrame({'Name':['adity','rabia','sharif','akib'],'roll':['34','01','27','22']})
+print(s1.head(3))
+print(s1.tail(3))
+print(s1.shape)
